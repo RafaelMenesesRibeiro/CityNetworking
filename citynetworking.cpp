f1ad614baf7a48fb071c757bc645613a1d7e1a3c
RafaelRibeiro97 <rafaelmmribeiro@gmail.com>
2017-04-22 13:50:39 -0700

    fixed kruskal's algorithm

--- citynetworking.cpp
+++ citynetworking.cpp
@@ -1,9 +1,9 @@
-// ASA Project 1
-// 2016/2017 - 2st Semestre
-// Instituto Superior Tecnico
-// U.C. Análise e Síntese de Algoritmos
-// Francisco Barros, nº 85069
-// Rafael Ribeiro, nº 84758
+//ASA Project 1
+//2016/2017 - 2st Semestre
+//Instituto Superior Tecnico
+//U.C. Análise e Síntese de Algoritmos
+//Francisco Barros, nº 85069
+//Rafael Ribeiro, nº 84758
 
 //Must be compiled with "g++ -std=c++11 ..."
 
@@ -46,32 +46,30 @@ int networkAirports = 0;
 
 ------------------------------------------------------------------------------*/
 
-// Connection represents a road or airway between city int:a to city int:b.
+//Connection represents a road or airway between city int:a to city int:b.
 typedef pair<int, int> Connection;
-// Edge is the pair of elements whose elements are a connection and the respective int:cost.
+//Edge is the pair of elements whose elements are a connection and the respective int:cost.
 typedef pair<Connection, int> Edge;
-// Graph
+
 
 /*------------------------------------------------------------------------------
 
 			AUXILIAR FUNCTIONS
 
 ------------------------------------------------------------------------------*/
-
-/** Comparator used for sorting the vector of concrete edges with vector::sort */
+//Comparator used for sorting the vector of concrete edges with vector::sort
 bool edgeWeightComparator(const Edge& edge, const Edge& anotherEdge) {
 	if (edge.second < anotherEdge.second) {
 		return true;
-	} else if (edge.second == anotherEdge.second) {
-		return false;
-	} else {
+	} 
+	else {
 		return false;
 	}
 }
 
-
-/** Prints the output in case of impossibility of creating a proper network. */
+//Prints the output in case of impossibility of creating a proper network.
 void outputInsuficient() { cout << "Insuficiente" << endl; }
+
 
 /*------------------------------------------------------------------------------
 
@@ -83,17 +81,16 @@ void outputInsuficient() { cout << "Insuficiente" << endl; }
 struct DisjointSets {
 	int *parent, *rnk;
 	int n;
-	DisjointSets(int n) {
-		this->n = n;
-		parent = new int[n];
-		rnk = new int[n];
-		for (int i = 0; i < n; i++) {
+	
+	DisjointSets(int vert) {
+		this->n = vert + 1;
+		rnk = new int[vert + 1];
+		parent = new int[vert + 1];
+		for (int i = 0; i < vert + 1; i++) {
 			rnk[i] = 0;
 			parent[i] = i;
 		}
 	}
-
-
 	int find(int u) {
 		if (u != parent[u]) {
 			parent[u] = find(parent[u]);
@@ -106,7 +103,7 @@ struct DisjointSets {
 		if (rnk[x] > rnk[y]) {
 			parent[y] = x;
 		}
-		else  {
+		else {
 			parent[x] = y;
 		}
 		if(rnk[x] == rnk[y]) {
@@ -118,15 +115,15 @@ struct DisjointSets {
 
 class Graph {
 private:
-		// Abstract city to which all graphVertices with an airport connect.
+	//Abstract city to which all graphVertices with an airport connect.
 	int skyCity;
-		// Number of vertices in the graph, that is to be turned into an MST.
+	//Number of vertices in the graph, that is to be turned into an MST.
 	int graphVertices;
-		// Total number of allowed networkMaxAirports in the graph.
+	//Total number of allowed networkMaxAirports in the graph.
 	int networkMaxAirports;
-		// Total number of allowed networkMaxRoads in the graph.
+	//Total number of allowed networkMaxRoads in the graph.
 	int networkMaxRoads;
-		// Listing of all concrete edges in the graph, edge = <connection, cost>.
+	//Listing of all concrete edges in the graph, edge = <connection, cost>.
 	vector<Edge> edgeVector;
 public:
 	Graph() {
@@ -137,79 +134,73 @@ public:
 	}
 	~Graph() {}
 
-		// Getters.
+	//Getters.
 	int getSkyCity() { return skyCity; }
 	int getGraphVertices() { return graphVertices; }
 	int getMaxAirports() { return networkMaxAirports; }
 	int getMaxRoads() { return networkMaxRoads; }
 
-		// Setters.
-	void setGraphVertices(int i) { graphVertices = i; }
+	//Setters.
+	void setGraphVertices(int i) {
+		this->skyCity = i + 1;
+		graphVertices = i;
+	}
 	void setMaxAirports(int i) { networkMaxAirports = i; }
 	void setMaxRoads(int i) { networkMaxRoads = i; }
 
-		// Adds the new adge to the graph.
-	void addEdge(Edge e) { edgeVector.push_back(e); }
-		// Sorts in crescent order the vector of concrete edges in this graph.
-	//void sortEdgeList() { edgeVector.sort(edgeWeightComparator); }
+	//Checks if the vertex has connections.
+	bool vertexCheckConnected(int city) {
+		for (vector<Edge>::const_iterator it = edgeVector.begin(); it != edgeVector.end(); it++) {
+			Connection connection = (*it).first; //Gets the connection of the edge.
+			int cityA = connection.first; //Gets one city.
+			int cityB = connection.second; //Gets the other city.
+			if (cityA == city || cityB == city) { //If any of the cities match the given one.
+				return true; //Returns true if it finds the given city as connections.
+			}
+		}
+		return false; //Returns false if the given city as no connections.
+	}
 
-		// Prints to standart output the information of a given edge.
-	void showEdge(Edge e) {
+	//Creates a new edge, <connection, cost>.
+	Edge newEdge(int a, int b, int c) {
+		Connection con = make_pair(a, b);
+		Edge e = make_pair(con, c);
+		return e;
+	}
+	//Adds the new adge to the graph.
+	void addEdge(Edge e) { edgeVector.push_back(e); }
+	//Prints to standard output the information of a given edge.
+	void printEdge(Edge e) {
 		Connection connection = e.first;
 		int connectionCost = e.second;
 		int cityA = connection.first;
 		int cityB = connection.second;
 		cout << "city a: " << cityA << ", city b: " << cityB << ", cost: " << connectionCost << endl;
 	}
-
-		// Prints to standart output the information of all edges in this graph.
+	//Prints to standart output the information of all edges in this graph.
 	void printEdgeList() {
 		cout << endl << "graph.printEdgeList()" << endl;
-		vector<Edge>::const_iterator ci;
-		for (ci = edgeVector.begin(); ci != edgeVector.end(); ++ci) {
-			showEdge(*ci);
+		for (vector<Edge>::const_iterator ci = edgeVector.begin(); ci != edgeVector.end(); ci++) {
+			printEdge(*ci);
 		}
 	}
 
-		int kruskalMST(); // Calculates the MST for this graph, generating the desired network.
-		
-		// Checks if the vertex has connections.
-		bool vertexCheckConnected(int city) {
-			vector<Edge>::const_iterator it;
-			for (it = edgeVector.begin(); it != edgeVector.end(); ++it) {
-				Connection connection = (*it).first; //Gets the connection of the edge.
-				int cityA = connection.first; //Gets one city.
-				int cityB = connection.second; //Gets the other city.
-				if (cityA == city || cityB == city) { //If any of the cities match the given one.
-					return true; //Returns true if it finds the given city as connections.
-				}
-			}
-			return false; //Returns false if the given city as no connections.
-		}
-
-		// Creates a new edge, <connection, cost>.
-		Edge newEdge(int a, int b, int c) {
-			Connection con = make_pair(a, b);
-			Edge e = make_pair(con, c);
-			return e;
-		}
-
-	};
+	int kruskalMST(); //Calculates the MST for this graph, generating the desired network.
+};
 
 int Graph::kruskalMST() {
-	int mst_wt = 0; 
-	sort(edgeVector.begin(), edgeVector.end(), edgeWeightComparator);
-	
-	printEdgeList();
-
+	int mst_wt = 0;
+	cout << "sets" << endl;
 	DisjointSets ds(getGraphVertices());
-	vector<Edge>::iterator it;
-	for (it = edgeVector.begin(); it != edgeVector.end(); ++it) {
+	cout << "sets2" << endl;
+	sort(edgeVector.begin(), edgeVector.end(), edgeWeightComparator);
+	printEdgeList();
+	for (vector<Edge>::const_iterator it = edgeVector.begin(); it != edgeVector.end(); it++) {
 		int u = (*it).first.first;
 		int v = (*it).first.second;
 		int set_u = ds.find(u);
 		int set_v = ds.find(v);
-		if(set_u != set_v) {
+		if (set_u != set_v) {
 			cout << u << " - " << v << endl;
 			mst_wt += (*it).second;
 			ds.merge(set_u, set_v);
@@ -217,11 +208,6 @@ int Graph::kruskalMST() {
 	}
 	return mst_wt;
 }
-
-
-
-
-
 
 
 /*------------------------------------------------------------------------------
@@ -239,9 +225,9 @@ int main() {
 	* int:c represents the cost of building an airport on city a or a road between a and b.
 	*/
 	int aux, a, b, c;
-	// Edge (auxiliar).
+	//Edge (auxiliar).
 	Edge e;
-	// Instanciate new graph with default constructor.
+	//Instanciate new graph with default constructor.
 	Graph graph;
 
 	/* Creation of the Graph */
@@ -266,14 +252,14 @@ int main() {
 		}
 	}
 
-	/* Insuficiency Verification	*/
-	// If the number os total connections isn't enough to connect the graph.
+	/* Insuficiency Verification */
+	//If the number os total connections isn't enough to connect the graph.
 	if (graph.getMaxRoads() + graph.getMaxAirports() < graph.getGraphVertices() - 1) {
 		outputInsuficient(); //Prints the output.
 		return 0; //Quits.
 	}
 
-	// Checks if all the vertices have at least one connection.
+	//Checks if all the vertices have at least one connection.
 	for (i = 1; i <= graph.getGraphVertices(); i++) {
 		if(!graph.vertexCheckConnected(i)) { //If the city has no connections.
 			outputInsuficient(); //Prints the output.
@@ -283,6 +269,6 @@ int main() {
 
 	/* Minimum Spanning Tree finding (Kruskal's algorithm) */
 	graph.printEdgeList();
-	int w = graph.kruskalMST();
-	cout << "custo" << w << endl;
+	int totalCost = graph.kruskalMST();
+	cout << "Custo Total: " << totalCost << endl;
 }
